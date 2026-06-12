9f7a7a80a03fdfc57f12ee174de20645fb0d5e21
987tom1 <987tom1@gmail.com>
2026-06-12 15:57:37 +1000

    debug: minimal handler with no imports to isolate export issue

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,5 @@
-import { createAppInstance } from '../src/app';
-
-const appPromise = createAppInstance();
-
 async function handler(req: any, res: any): Promise<void> {
-  const app = await appPromise;
-  app(req, res);
+  res.json({ ok: true, service: 'connection-made-simple' });
 }
 
 export = handler;
